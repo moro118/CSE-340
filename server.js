9ed29242da79065bad64c38b42674b999e365cd5
moro118 <mvillalobosortega@byupathway.edu>
2026-05-23 03:46:09 -0600

    Update README.md with project description and usage instructions

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
+const NODE_ENV = "production";
+const PORT=3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+/**
+ * Routes
+ */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home.ejs', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations.ejs', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects.ejs', { title });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+});
