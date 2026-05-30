ed9e8e5eca0ecd3268d2cd0652ab007ec3e94edf
moro118 <mvillalobosortega@byupathway.edu>
2026-05-30 15:02:27 -0600

    feat: implement front-end UI and CSS styling for homepage, projects, and categories views

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { fileURLToPath } from 'url';
 import path from 'path';
-const NODE_ENV = "production";
-const PORT=3000;
+const nodeEnv = process.env.NODE_ENV || "production";
+const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -24,17 +24,19 @@ app.get('/organizations', async (req, res) => {
     res.render('organizations.ejs', { title });
 });
 
-    app.get('/projects', async (req, res) => {
-        const title = 'Service Projects';
-        res.render('projects.ejs', { title });
-    });
-    
-    app.get('/categories', async (req, res) => {
-        const title = 'Service Categories';
-        res.render('categories.ejs', { title });
-    });
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects.ejs', { title });
 });
+
+app.get('/categories', async (req, res) => {
+    const title = 'Service Categories';
+    res.render('categories.ejs', { title });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://127.0.0.1:${port}`);
+    console.log(`Environment: ${nodeEnv}`);
+});
+
+
